345ffb45b70d48afda3b81c202a500ec0c46c983
ognevoydev <ognevoy.devmail@gmail.com>
2023-01-09 16:12:53 +0300

    clean up

---
+++ data/build/generated/source/buildConfig/debug/com/incomee/incomee/data/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.incomee.incomee.data;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.incomee.incomee.data";
+  public static final String BUILD_TYPE = "debug";
+}
