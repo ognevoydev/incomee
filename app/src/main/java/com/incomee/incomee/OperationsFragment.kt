fa6f990ae4498c83e740713a3c2b5079ba33243e
ognevoydev <109038517+ognevoydev@users.noreply.github.com>
2023-01-14 13:53:45 +0300

    implement operations screen

---
+++ app/src/main/java/com/incomee/incomee/OperationsFragment.kt
@@ -0,0 +1,37 @@
+package com.incomee.incomee
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.incomee.incomee.adapter.FiltersAdapter
+import kotlinx.android.synthetic.main.fragment_operations.*
+
+
+class OperationsFragment : Fragment(R.layout.fragment_operations) {
+
+    private lateinit var filtersAdapter: FiltersAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_operations, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        filtersAdapter = FiltersAdapter()
+        val layoutManager: LayoutManager = LinearLayoutManager(context, HORIZONTAL, false)
+        filtersRecyclerView.apply {
+            adapter = filtersAdapter
+            setLayoutManager(layoutManager)
+        }
+
+    }
+
+
+}
