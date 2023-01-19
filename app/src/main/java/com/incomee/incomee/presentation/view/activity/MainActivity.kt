51b46cc3c747b8d62c757fa065002f2344d01299
ognevoydev <ognevoy.devmail@gmail.com>
2023-01-19 20:05:41 +0300

    disable cross fade animation

--- app/src/main/java/com/incomee/incomee/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/incomee/incomee/presentation/view/activity/MainActivity.kt
@@ -3,15 +3,12 @@ package com.incomee.incomee.presentation.view.activity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.TooltipCompat
 import androidx.core.view.forEach
-import androidx.fragment.app.add
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.incomee.incomee.R
 import com.incomee.incomee.databinding.ActivityMainBinding
-import com.incomee.incomee.presentation.view.fragment.OperationsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,15 +18,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = this.findNavController(R.id.fragment_container_view)
-        b.bottomNavigationBar.setupWithNavController(navController)
-
         disableBottomBarTooltips()
+        val navController = this.findNavController(R.id.fragment_container)
+        b.bottomNavigationBar.setupWithNavController(navController)
+        b.bottomNavigationBar.selectedItemId = (R.id.operationsFragment)
+
     }
 
-    fun disableBottomBarTooltips() {
-        b.bottomNavigationBar
-            .menu.forEach {
+    private fun disableBottomBarTooltips() {
+        b.bottomNavigationBar.menu.forEach {
                 b.bottomNavigationBar.findViewById<View>(it.itemId).setOnLongClickListener {
                     true
                 }
