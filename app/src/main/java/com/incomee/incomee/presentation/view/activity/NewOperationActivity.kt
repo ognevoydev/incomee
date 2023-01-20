620e171eaf4b2773f7636295ca5ed7aa665c0058
ognevoydev <ognevoy.devmail@gmail.com>
2023-01-20 12:07:13 +0300

    refactor new operation screen

--- app/src/main/java/com/incomee/incomee/presentation/view/activity/NewOperationActivity.kt
+++ app/src/main/java/com/incomee/incomee/presentation/view/activity/NewOperationActivity.kt
@@ -2,11 +2,19 @@ package com.incomee.incomee.presentation.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
 import com.incomee.incomee.R
+import com.incomee.incomee.presentation.view.fragment.NewExpenseFragment
+import com.incomee.incomee.presentation.view.fragment.OperationsFragment
 
 class NewOperationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_operation)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add<NewExpenseFragment>(R.id.fragment_container_view).commit()
+        }
     }
 }
